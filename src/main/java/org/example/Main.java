aceb37f0d5248db093b8ea6c2c067599616ce83b
Anton248 <31551544+Anton248@users.noreply.github.com>
2023-03-18 13:16:15 +0300

    3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,13 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+//import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Selenide.open("https://www.google.com/");
     }
 }
