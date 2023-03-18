6a10bf067766d814a7974538f815488348fc27c9
Anton248 <31551544+Anton248@users.noreply.github.com>
2023-03-18 13:17:18 +0300

    4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-//import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
