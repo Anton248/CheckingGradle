144f00ff125f5c3fdb05b1f53daf957f597be5ca
Anton248 <31551544+Anton248@users.noreply.github.com>
2023-03-18 12:13:51 +0300

    Initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
